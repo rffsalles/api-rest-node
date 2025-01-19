0495bcd07b2dbbbaa7d21423533bbdd4f18db8e5
Ricardo Salles <ricardo@unovo.com.br>
2025-01-19 09:25:04 -0300

    Vitest

---
+++ test/example.spec.ts
@@ -0,0 +1,7 @@
+import {expect,test} from 'vitest';
+
+test('O usuário consegue criar uma nova transação', () => {
+  
+    const responseStatusCode = 200
+    expect(responseStatusCode).toEqual(200)
+});
