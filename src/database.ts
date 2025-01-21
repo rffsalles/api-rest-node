64c3a30ec87836eabff5a86fa4f9f6096935adf9
Ricardo Salles <ricardo@unovo.com.br>
2025-01-20 22:58:16 -0300

    Change database for test

--- src/database.ts
+++ src/database.ts
@@ -6,6 +6,7 @@ if (!process.env.DATABASE_URL) {
   throw new Error('DATABASE_URL must be set')
 }
 
+
 export const config: Knex.Config = {
   client: 'sqlite3',
   connection: {
--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -1,13 +1,19 @@
-import {it,beforeAll, afterAll, describe} from 'vitest';
+import {it,beforeAll, afterAll, describe, expect, beforeEach} from 'vitest';
+import {exec, execSync} from 'child_process';
 import request from 'supertest';
 import {app} from '../src/app';
 
 describe('Transactions Routes', () => {
-  beforeAll(async () => {
+  beforeAll(async () => {    
     await app.ready();
   })
   afterAll (async () => {
     await app.close();
+  })
+
+  beforeEach(() => {
+    execSync('npm run knex migrate:rollback --all')
+    execSync('npm run knex migrate:latest')
   })
 
   it('should be able to create a new transaction', async () => {
@@ -32,10 +38,17 @@ describe('Transactions Routes', () => {
       .expect(201);    
       const cookies = createTransactionResponse.get('Set-Cookie');
       if (cookies) {
-        await request(app.server)
+        const listTransactionsResponse = await request(app.server)
           .get('/transactions')
           .set('Cookie', cookies)
-          .expect(200);
+          .expect(200)
+
+          expect(listTransactionsResponse.body.transactions).toEqual([
+            expect.objectContaining({
+              title: 'New Transaction',
+              amount: 5000,
+            })
+          ])
       }
    })
 
